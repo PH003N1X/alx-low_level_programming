9183a2651a57834e81945c163ae1f348b5e137b0
PH003N1X <ahmede.mostafa5@gmail.com>
2023-06-20 14:15:10 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: checks for function input.
+ * @b: checks for function input.
+ *
+ * Return: c.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
