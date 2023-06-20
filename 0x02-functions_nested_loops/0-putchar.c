2bb3ac8f7f827cd2793a18513a47f86c751d68a3
PH003N1X <ahmede.mostafa5@gmail.com>
2023-06-20 12:46:02 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stout
+ *
+ * Reutrn: on success 1.
+ *         on error -1 is returned.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
