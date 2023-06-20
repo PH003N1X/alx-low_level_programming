548eba956b0fb61ece1186bc4964a7f9bbd5bd05
PH003N1X <ahmede.mostafa5@gmail.com>
2023-06-20 12:58:42 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabetical letters in lower case
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
